f76ff30536a4bb354c39eb6e948c55b66634e999
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-11-14 17:54:27 +0000

    Optimize imports

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-from PyQt5 import QtCore
-from pytest_anki import AnkiSession, AnkiWebViewType
+from pytest_anki import AnkiSession
 from selenium import webdriver
-from aqt import importing
 from aqt.main import AnkiQt
-import pytest
 
 def test_my_addon(anki_session: AnkiSession, monkeypatch):
     def dummy_func(): pass
     monkeypatch.setattr(AnkiQt, "handleImport", dummy_func)
 
-    my_addon = anki_session.load_addon("myaddon")
     with anki_session.profile_loaded():
         from myaddon import action
         action.trigger()
