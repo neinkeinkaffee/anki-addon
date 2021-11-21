c56a7869ff9857844c1b41996eda654092c8af5e
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-11-21 13:46:35 +0000

    Rename variables and functions

--- myaddon/__init__.py
+++ myaddon/__init__.py
@@ -3,6 +3,6 @@ from aqt.qt import *
 
 from myaddon.embedded_browser import show_mini_browser
 
-action = QAction("test", mw)
-qconnect(action.triggered, show_mini_browser)
-mw.form.menuTools.addAction(action)
+open_addon_window = QAction("myaddon", mw)
+qconnect(open_addon_window.triggered, show_mini_browser)
+mw.form.menuTools.addAction(open_addon_window)
--- test.py
+++ test.py
@@ -6,21 +6,25 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 def test_copy_text_to_back(anki_session: AnkiSession, monkeypatch):
-    def dummy_func(*args, **kwargs): pass
-    monkeypatch.setattr(AnkiQt, "handleImport", dummy_func)
+    prevent_import_window_from_opening(monkeypatch)
 
     with anki_session.profile_loaded():
-        addon = anki_session.load_addon(package_name="myaddon")
-        addon.action.trigger()
+        myaddon = anki_session.load_addon(package_name="myaddon")
+        myaddon.open_addon_window.trigger()
 
         def select_text_on_webpage(driver: webdriver.Chrome):
-            actionChains = ActionChains(driver)
             test_span = driver.find_element_by_xpath("//span[contains(text(), 'test')]")
+            actionChains = ActionChains(driver)
             actionChains.double_click(test_span).perform()
-            browser_tab = anki_session.mw.findChild(addon.browser_tab.BrowserTab)
+            browser_tab = anki_session.mw.findChild(myaddon.browser_tab.BrowserTab)
             browser_tab.copy_to_back_action.trigger()
             def selected_text_copied_to_back():
                 assert browser_tab.editor.note.fields[1] == "test"
             anki_session.qtbot.waitUntil(selected_text_copied_to_back)
 
         anki_session.run_with_chrome_driver(select_text_on_webpage)
+
+def prevent_import_window_from_opening(monkeypatch):
+    def dummy_func(*args, **kwargs): pass
+
+    monkeypatch.setattr(AnkiQt, "handleImport", dummy_func)
