39830297e6caae5d8c6bc87778f1dbedb025435a
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-12-05 20:57:36 +0000

    Move all spikes into spikes directory

--- spikes/addon/myaddon/__init__.py
+++ spikes/addon/myaddon/__init__.py
@@ -1,7 +1,7 @@
 from aqt import mw
 from aqt.qt import *
 
-from myaddon.browser.embedded_browser import show_mini_browser
+from .browser.embedded_browser import show_mini_browser
 
 open_addon_window = QAction("myaddon", mw)
 qconnect(open_addon_window.triggered, show_mini_browser)
