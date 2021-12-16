0df2b3472c31377178e5cca9d279944e9ca3fe11
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-12-16 11:46:12 +0000

    Remove some blank lines

--- spikes/pytest-qt/browser.py
+++ spikes/pytest-qt/browser.py
@@ -1,29 +1,22 @@
-#!/usr/bin/python
-
 import sys
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (QApplication, QLineEdit, QMainWindow,
-                             QPushButton, QToolBar)
+from PyQt5.QtWidgets import (QApplication, QLineEdit, QMainWindow, QPushButton, QToolBar)
 from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
 
 
 class Browser(QMainWindow):
-
     def __init__(self):
         super(Browser, self).__init__()
 
         self.initUI()
 
-
     def initUI(self):
-
         self.toolBar = QToolBar(self)
         self.addToolBar(self.toolBar)
 
         self.backBtn = QPushButton(self)
         self.backBtn.setEnabled(False)
-
         self.backBtn.setIcon(QIcon(':/qt-project.org/styles/commonstyle/images/left-32.png'))
         self.backBtn.clicked.connect(self.back)
         self.toolBar.addWidget(self.backBtn)
@@ -31,7 +24,6 @@ class Browser(QMainWindow):
         self.forBtn = QPushButton(self)
         self.forBtn.setEnabled(False)
         self.forBtn.setIcon(QIcon(':/qt-project.org/styles/commonstyle/images/right-32.png'))
-
         self.forBtn.clicked.connect(self.forward)
         self.toolBar.addWidget(self.forBtn)
 
@@ -41,9 +33,7 @@ class Browser(QMainWindow):
 
         self.webEngineView = QWebEngineView(self)
         self.setCentralWidget(self.webEngineView)
-
         self.webEngineView.page().urlChanged.connect(self.onLoadFinished)
-
         self.webEngineView.page().titleChanged.connect(self.setWindowTitle)
         self.webEngineView.page().urlChanged.connect(self.urlChanged)
 
@@ -52,7 +42,6 @@ class Browser(QMainWindow):
         self.show()
 
     def onLoadFinished(self):
-
         if self.webEngineView.history().canGoBack():
             self.backBtn.setEnabled(True)
         else:
@@ -63,11 +52,8 @@ class Browser(QMainWindow):
         else:
             self.forBtn.setEnabled(False)
 
-
     def load(self):
-
         url = QUrl.fromUserInput(self.address.text())
-
         if url.isValid():
             self.webEngineView.load(url)
 
@@ -82,11 +68,9 @@ class Browser(QMainWindow):
 
 
 def main():
-
     app = QApplication(sys.argv)
     ex = Browser()
     sys.exit(app.exec_())
-
 
 
 if __name__ == '__main__':
