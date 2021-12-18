2849c3b61c6ae216a93944456622c3520e267f0c
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-12-18 16:12:59 +0000

    Rename variable for object under test

--- embedded_browser/test/test_browser.py
+++ embedded_browser/test/test_browser.py
@@ -28,40 +28,40 @@ def test_loads_page_and_navigates_back_and_forth(qtbot, monkeypatch):
 
 
 def test_opens_new_tab_with_blank_default_page(qtbot, monkeypatch):
-    browser_driver = BrowserDriver(qtbot, monkeypatch)
+    browser = BrowserDriver(qtbot, monkeypatch)
 
-    browser_driver.open_new_tab()
+    browser.open_new_tab()
 
-    browser_driver.assert_tab_count(2)
-    browser_driver.assert_address_bar_contains("about:blank")
-    browser_driver.assert_active_browser_tab_title("about:blank")
+    browser.assert_tab_count(2)
+    browser.assert_address_bar_contains("about:blank")
+    browser.assert_active_browser_tab_title("about:blank")
 
 
 def test_backward_and_forward_buttons_are_tab_sensitive(qtbot, monkeypatch):
-    browser_driver = BrowserDriver(qtbot, monkeypatch)
-    browser_driver.assert_backward_button_disabled()
+    browser = BrowserDriver(qtbot, monkeypatch)
+    browser.assert_backward_button_disabled()
 
-    browser_driver.open_new_tab()
-    browser_driver.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_1.html")
-    browser_driver.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_2.html")
-    browser_driver.assert_backward_button_enabled()
-    browser_driver.switch_to_tab(0)
-    browser_driver.assert_backward_button_disabled()
+    browser.open_new_tab()
+    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_1.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_2.html")
+    browser.assert_backward_button_enabled()
+    browser.switch_to_tab(0)
+    browser.assert_backward_button_disabled()
 
-    browser_driver.switch_to_tab(1)
-    browser_driver.click_backward_button()
-    browser_driver.assert_forward_button_enabled()
-    browser_driver.switch_to_tab(0)
-    browser_driver.assert_forward_button_disabled()
+    browser.switch_to_tab(1)
+    browser.click_backward_button()
+    browser.assert_forward_button_enabled()
+    browser.switch_to_tab(0)
+    browser.assert_forward_button_disabled()
 
 def test_closes_tab(qtbot, monkeypatch):
-    browser_driver = BrowserDriver(qtbot, monkeypatch)
+    browser = BrowserDriver(qtbot, monkeypatch)
 
-    browser_driver.open_new_tab()
-    browser_driver.assert_tab_count(2)
+    browser.open_new_tab()
+    browser.assert_tab_count(2)
 
-    browser_driver.close_active_tab()
-    browser_driver.assert_tab_count(1)
+    browser.close_active_tab()
+    browser.assert_tab_count(1)
 
-    browser_driver.close_active_tab()
-    browser_driver.assert_tab_count(1)
+    browser.close_active_tab()
+    browser.assert_tab_count(1)
