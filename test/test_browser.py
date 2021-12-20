a170664bb2e820cb856b7b2259dcfc50ffa93958
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-12-20 16:01:14 +0000

    Move test directory up one level

--- test/test_browser.py
+++ test/test_browser.py
@@ -1,17 +1,20 @@
+import os
+
 from .browser_driver import BrowserDriver
 
-FIXTURES_DIR = "/Users/gesa/repos/anki-addon/embedded_browser/test/fixtures"
+
+FIXTURES_PATH = f"file:///{os.getcwd()}/test/fixtures"
 
 
 def test_loads_page_and_navigates_back_and_forth(qtbot):
     browser = BrowserDriver(qtbot)
 
-    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_1.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_PATH}/page_1.html")
     browser.assert_address_bar_contains("page_1.html")
     browser.assert_active_browser_tab_contains_html("This is a test")
     browser.assert_active_browser_tab_title("Test Page")
 
-    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_2.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_PATH}/page_2.html")
     browser.assert_address_bar_contains("page_2.html")
     browser.assert_active_browser_tab_contains_html("This is another test")
     browser.assert_active_browser_tab_title("Another Test Page")
@@ -42,8 +45,8 @@ def test_backward_and_forward_buttons_are_tab_sensitive(qtbot):
     browser.assert_backward_button_disabled()
 
     browser.open_new_tab()
-    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_1.html")
-    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_2.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_PATH}/page_1.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_PATH}/page_2.html")
     browser.assert_backward_button_enabled()
     browser.switch_to_tab(0)
     browser.assert_backward_button_disabled()
@@ -71,7 +74,7 @@ def test_closes_tabs_but_keeps_last_tab_open(qtbot):
 def test_calls_callback_on_clicking_context_menu_action(qtbot):
     browser = BrowserDriver(qtbot)
 
-    browser.enter_address_and_hit_return(f"{FIXTURES_DIR}/page_with_test_span.html")
+    browser.enter_address_and_hit_return(f"{FIXTURES_PATH}/page_with_test_span.html")
     browser.select_test_span_and_trigger_copy_to_card_action()
 
     browser.assert_context_menu_callback_called_with_expected_text("selection test")
